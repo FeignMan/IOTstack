80f0aaf3fe23d21d3259f0ce376066300d867c33
Ishan Aditya <ishanaditya@gmail.com>
2020-11-26 02:03:05 +0100

    [sonarr] Added Sonarr to the mix

--- menu.sh
+++ menu.sh
@@ -9,6 +9,7 @@ declare -A cont_array=(
 	[deluge]="Deluge"
 	[jackett]="Jackett"
 	[radarr]="Radarr"
+        [sonarr]="Sonarr"
 	[nodered]="Node-RED"
 	[influxdb]="InfluxDB"
 	[telegraf]="Telegraf (Requires InfluxDB and Mosquitto)"
@@ -34,7 +35,7 @@ declare -A cont_array=(
 	[python]="Python 3"
 
 )
-declare -a armhf_keys=("portainer" "traefik" "deluge" "jackett" "radarr" "nodered" "influxdb" "grafana" "mosquitto" "telegraf" "mariadb" "postgres"
+declare -a armhf_keys=("portainer" "traefik" "deluge" "jackett" "radarr" "sonarr" "nodered" "influxdb" "grafana" "mosquitto" "telegraf" "mariadb" "postgres"
 	"adminer" "openhab" "zigbee2mqtt" "pihole" "plex" "tasmoadmin" "rtl_433" "espruinohub"
 	"motioneye" "webthings_gateway" "blynk_server" "nextcloud" "diyhue" "homebridge" "python")
 
